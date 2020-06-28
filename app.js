a9a79d6860caeeeaa66786fd3fa1d0e3ba32103a
vermmatt <47433305+vermmatt@users.noreply.github.com>
2020-06-28 22:33:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// var data = {
+//     date: Number,
+//     day: Number,
+//     hour: Number,
+//     minute: Number
+// }
+
+var data = [];
+var caffeine = 100;
+
+
+var time = new Date();
+var month = Number(time.getMonth());
+var day = Number(time.getDate());
+var hours =  Number(time.getHours());
+
+
+// loop through array
+for(i=0; i < 24; i++){
+    //add hours
+    var obj = [];
+    if(hours<24){
+        var timestamp = month.toString() + "-" + day.toString() + "-" + hours.toString()
+        obj.push(timestamp, caffeine);
+        data.push(obj);
+        hours++
+        caffeine = caffeine * 0.89089;
+    } else {
+        hours = 0;
+        day += 1;
+        var timestamp = month.toString() + "-" + day.toString() + "-" + hours.toString()
+        obj.push(timestamp, caffeine);
+        data.push(obj);
+        hours++;
+        caffeine = caffeine * 0.89089;
+    };
+}
+console.log(data);
+
